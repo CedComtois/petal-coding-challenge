560e3828dc1973b345b8ea5cc41d8a8ca8e5d3e7
Cedrik Comtois <cedrik.comtois@gmail.com>
2023-02-21 13:50:44 -0500

    used gist directly in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
 
-require 'csv'
-
-csv_file_path ||= File.read("./db/pokemon.csv")
-csv ||= CSV.parse(csv_file_path, col_sep: ",", row_sep: :auto, skip_blanks: true)
+csv_text = URI.open("https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv")
+csv = CSV.parse(csv_text, headers: true, col_sep: ",", row_sep: :auto, skip_blanks: true)
 
 pokemon_values = []
 
